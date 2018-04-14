a3c920a9eec4ef0761d1a8d44a457b51d4126216
lior gishry <liorgish@gmail.com>
2018-04-14 22:49:41 +0300

    more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from './app.component'
+import {TabsPage} from "../pages/tabs/tabs";
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +20,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    TabsPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+import {IonicPage, NavController} from "ionic-angular";
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html'
+})
+
+export class TabsPage {
+  constructor(private navController: NavController) {
+
+  }
+}
